b1e737fe7d6f0e27b0c1fda5a1e103b97ff1245b
Thomas Alves <talvesdev@outlook.com>
2018-03-10 22:46:51 -0300

    Separating the layout in React components

---
+++ src/components/SearchBoxComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class SearchBoxComponent extends React.Component {
+	render() {
+		return (
+			<div className="toolbar">
+				<div className="toolbar__form">
+					<input type="text" className="search-job" placeholder="The most delicious jobs for designers"/>
+				</div>
+				<button className="search-job-button">
+					<i className="fa fa-search"></i>
+				</button>
+			</div>
+		)
+	}
+}
+
+export default SearchBoxComponent;
