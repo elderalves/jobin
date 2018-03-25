85df283f5b5e2608a4ec47baa263c42ee72120d8
Thomas Alves <talvesdev@outlook.com>
2018-03-24 22:38:02 -0300

    Fixing job items card layout

--- src/components/JobItemComponent.js
+++ src/components/JobItemComponent.js
@@ -1,8 +1,11 @@
 import React from "react";
+import appleImage from "../images/apple-logo.png";
 
 const JobItemComponent = () => (
 	<div className="job-item">
-		<div className="job-item__logo">APPLE :)</div>
+		<div className="job-item__logo">
+			<img src={appleImage} alt="Apple" className="job__logo-image" />
+		</div>
 		<div className="job-item__content">
 			<div className="job-item__title">Ultra Developer</div>
 			<div className="job-item__text">
