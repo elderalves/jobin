30741927c8e49e4215003d7d06346ed62a24f03e
Michael Alves <michaelalves@outlook.com>
2018-03-27 07:12:41 -0300

    test husky

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 import SearchBox from "./components/SearchBox";
 import JobsList from "./components/JobsList";
@@ -9,15 +9,15 @@ class App extends Component {
   render() {
     return (
       <div className="main">
-      	<header>
-      		<div className="logo">JOB</div>
-      	</header>
-      	<main>
-      		<div className="container">
-						<SearchBox />
+        <header>
+          <div className="logo">JOB</div>
+        </header>
+        <main>
+          <div className="container">
+            <SearchBox />
             <JobsList />
-      		</div>
-      	</main>
+          </div>
+        </main>
       </div>
     );
   }
